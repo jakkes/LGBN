5c412952d56be1bae3199bd51db3d7c37263af46
Jakob Stigenberg <jakob@jakke.se>
2021-04-11 21:03:56 +0200

    add variable names

--- pgm/distributions/conditional/_base.py
+++ pgm/distributions/conditional/_base.py
@@ -1,4 +1,5 @@
 import abc
+from typing import Sequence, List
 
 import numpy as np
 
@@ -6,6 +7,24 @@ import pgm.distributions as distributions
 
 class Base(abc.ABC):
     """Base conditional distribution."""
+
+    def __init__(self, variable_names: Sequence[str]):
+        """
+        Args:
+            variable_names (Sequence[str]): Sequence of variable name identifiers.
+                Output variables are ordered, and thus identifiable, by these names.
+        """
+        self._variable_names = list(variable_names)
+
+    @property
+    def dim(self) -> int:
+        """Dimension in which the distribution generates samples."""
+        return len(self._variable_names)
+
+    @property
+    def variable_names(self) -> List[str]:
+        """Variable names of outputs."""
+        return self._variable_names
 
     @abc.abstractmethod
     def marginalize(self, distribution: distributions.Base) -> distributions.Base:
